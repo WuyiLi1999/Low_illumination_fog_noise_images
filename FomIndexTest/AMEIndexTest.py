4ccf873e7f8010bee948d73ecc278a3f0859b3f5
liwuyi <1666071726@qq.com>
2023-12-21 16:42:48 +0800

    RAME评价指标提交

---
+++ FomIndexTest/AMEIndexTest.py
@@ -0,0 +1,26 @@
+import numpy as np
+from PIL import Image
+
+def calculate_ame(original_image_path, enhanced_image_path):
+    # 读取原始图像和增强后的图像
+    original_image = Image.open(original_image_path)
+    enhanced_image = Image.open(enhanced_image_path)
+
+    # 转换为灰度图像
+    original_image_gray = original_image.convert('L')
+    enhanced_image_gray = enhanced_image.convert('L')
+
+    # 将图像转换为NumPy数组
+    original_array = np.array(original_image_gray)
+    enhanced_array = np.array(enhanced_image_gray)
+
+    # 计算绝对误差，并对所有像素求和
+    abs_error = np.abs(original_array - enhanced_array)
+    ame = np.mean(abs_error)
+
+    return ame
+originalPath = "../resultImages/waterwall/waterfall.jpg"
+enhancePath = "../resultImages/waterwall/waterfall[21].jpg"
+
+result = calculate_ame(originalPath,enhancePath)
+print(result)
